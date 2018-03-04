b5502b59de9db1e2b2b9d08351226c520ca3881e
Arsereg <contacto@ginomarin.com>
2018-03-04 15:41:25 -0600

    Repaired "Consulta" types from String to int

--- Entities-POJO/Consulta.cs
+++ Entities-POJO/Consulta.cs
@@ -8,8 +8,8 @@ namespace Entities_POJO
 {
     public class Consulta : BaseEntity
     {
-        public string Id_Consulta { get; set; }
-        public string Id_traduccion { get; set; }
+        public int Id_Consulta { get; set; }
+        public int Id_traduccion { get; set; }
         public string Nombre_usuario { get; set; }
 
         public Consulta()
